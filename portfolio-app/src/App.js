c528896db359b72d3b473e6152388dec4dc31b91
Nathan Yuan <nathanyuan00@icloud.com>
2023-10-20 17:50:44 -0700

    converted nav bar on old index.html into a react component

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -22,7 +22,7 @@ function App() {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
     </div>
   );
 }
--- portfolio-app/src/navbar.js
+++ portfolio-app/src/navbar.js
@@ -4,10 +4,11 @@ import './styles.css'; // use the nav bar css from styles css
 // nav bar component 
 const Navbar = () => {
     return (
+        // nav bar
         <nav>
             <ul className="navbar">
                 <li><a href="index.html">Nathan Yuan</a></li>
-                <li><a href="projects.html">Technical Projects</a></li>
+                <li><a href="projects.html">Technical Projects</a></li> 
                 <li><a href="work.html">Work Experience</a></li>
                 <li><a href="education.html">Education & Leadership</a></li>
             </ul>
