476dd036d205ca4d33e2ee605bfe9bb744915629
Nathan Yuan <nathanyuan@Nasians-Macbook-Pro.local>
2024-01-07 19:47:20 -0800

    added more of my projects

--- portfolio-app/src/project-data.js
+++ portfolio-app/src/project-data.js
@@ -13,8 +13,16 @@ export const dataP = [
   desc:
 "- Worked within a team of 6 to develop a carpool py4web app for student users near campus to coordinate travel \n\n- Developed a list of available drivers or riders using python and vue.js for back end \n\n- Implemented google map view using javascript api, direct message feature, and edit profile/schedule feature"
   },
-  { name: "Portfolio Website", title: "Front End Engineer", skills: "React js, HTML, CSS, JS, , Vue.js, Github Pages",
+  { name: "Portfolio Website", title: "Front End Engineer", skills: "React js, HTML, CSS, JS, Vue.js, Github Pages",
   desc:
-"  - Developed this static website to house my portfolio and learn react js \n\n- null"
+"  - Developed this static website to house my portfolio and learn react js \n\n- Utilized github pages to host static website \n\n- Learned the correct Google domain anme system setup to have a custom domain name for this site"
+    },
+  { name: "Linktree Inspired Website", title: "Front End Engineer", skills: "HTML, CSS, JS, Github Pages",
+  desc:
+"  - Developed a static website with a landing page that has a list of widgets which link to whatever a user wants  \n\n- Implemented a copy link to clipboard button feature that doesnt also click the widget link itself \n\n- Developed different desktop and mobile friendly views"
+      },
+  { name: "Video Game Modding", title: "Software Engineer", skills: "Lua, XML",
+  desc:
+"  - Studied game files to learn their tech stack and how features were organized and developed \n\n- Edited code to alter the game experience to what I wanted \n\n- Changed Ai personalities and other paramters to better suit my needs"
     },
 ];
