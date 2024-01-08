fe0cc64510a9a7fdc1340452f25b70df69ab24fe
Nathan Yuan <nathanyuan@Nasians-Macbook-Pro.local>
2024-01-07 20:28:00 -0800

    updated some sizing to scale with view, and updated skill icons

--- portfolio-app/src/skill-data.js
+++ portfolio-app/src/skill-data.js
@@ -1,22 +1,38 @@
 // all data for my skills
 import cartier from './src images/cartier.png'
 
-export const dataS = [
-    { skills: "Python", img: [cartier, false]},
-    { skills: "CSS", img: [cartier, false]},
-    { skills: "HTML", img: [cartier, false]},
-    { skills: "Javascript", img: [cartier, false]},
-    { skills: "C", img: [cartier, false]},
-    { skills: "C++", img: [cartier, false]},
-    { skills: "Github", img: [cartier, false]},
-    { skills: "Terminal", img: [cartier, false]},
-    { skills: "Command prompt", img: [cartier, false]},
-    { skills: "APIs", img: [cartier, false]},
-    { skills: "SQL", img: [cartier, false]},
-    { skills: "Swift", img: [cartier, false]},
-    { skills: "SwiftUI", img: [cartier, false]},
-    { skills: "LaTeX", img: [cartier, false]},
-    { skills: "Search engine optimization", img: [cartier, false]},
-    { skills: "MIPS Assembly", img: [cartier, false]},
+import python from './src images/python.png'
+import css from './src images/css.png'
+import html from './src images/html.png'
+import js from './src images/js.png'
+// import C from './src images/C.png'
+// import cPlusPlus from './src images/C++.png'
+// import Github from './src images/Github.png'
+// import Terminal from './src images/Terminal.png'
+// import CommandPrompt from './src images/Command prompt.png'
+// import APIs from './src images/APIs.png'
+// import SQL from './src images/SQL.png'
+// import Swift from './src images/Swift.png'
+// import SwiftUI from './src images/SwiftUI.png'
+// import LaTeX from './src images/LaTeX.png'
+// import SEO from './src images/Search engine optimization.png'
+// import MIPS from './src images/MIPS Assembly.png'
 
+export const dataS = [
+    { tag: "Python", img: [python, false]},
+    { tag: "CSS", img: [css, false]},
+    { tag: "html", img: [html, false]},
+    { tag: "JavaScript", img: [js, false]},
+    { tag: "C", img: [cartier, false]},
+    { tag: "C++", img: [cartier, false]},
+    { tag: "Github", img: [cartier, false]},
+    { tag: "Terminal", img: [cartier, false]},
+    { tag: "Command prompt", img: [cartier, false]},
+    { tag: "APIs", img: [cartier, false]},
+    { tag: "SQL", img: [cartier, false]},
+    { tag: "Swift", img: [cartier, false]},
+    { tag: "SwiftUI", img: [cartier, false]},
+    { tag: "LaTeX", img: [cartier, false]},
+    { tag: "Search engine optimization", img: [cartier, false]},
+    { tag: "MIPS Assembly", img: [cartier, false]},
 ];
