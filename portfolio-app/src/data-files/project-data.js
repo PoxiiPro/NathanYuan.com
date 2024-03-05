2fdd5b4b2f8dcc34fe129541347cb4868a9fc716
Nathan Yuan <nathanyuan00@icloud.com>
2024-03-05 13:01:25 -0800

    some project text updates

--- portfolio-app/src/data-files/project-data.js
+++ portfolio-app/src/data-files/project-data.js
@@ -9,15 +9,15 @@ export const dataP = [
   desc:
   "- Worked within a team of three to develop an ai defensive and offensive agent that operated simultaneously \n\n- Competed against other team developed ai defensive and offensive agents \n\n- Developed reflex, minimax, alpha beta, expectimax, q learning, value iteration, and dfs/bfs pacman search agents by programming models/algorithms and training them" 
   },
-  { name: "Carpool Website", title: "Fullstack Software Engineer", skills: "PY4WEB, HTML, CSS, Bulma, JS, Python, Vue.js, Goole Maps js API",
+  { name: "Carpool Webapp", title: "Fullstack Software Engineer", skills: "PY4WEB, HTML, CSS, Bulma, JS, Python, Vue.js, Goole Maps js API",
   desc:
 "- Worked within a team of 6 to develop a carpool py4web app for student users near campus to coordinate travel \n\n- Developed a list of available drivers or riders using python and vue.js for back end \n\n- Implemented google map view using javascript api, direct message feature, and edit profile/schedule feature"
   },
-  { name: "Portfolio Website", title: "Front End Engineer", skills: "React.js, HTML, CSS, JS, Github Pages, Firebase, chatGPT API",
+  { name: "Portfolio Website", title: "Fullstack Engineer", skills: "React.js, HTML, CSS, JS, Github Pages, Firebase, chatGPT API",
   desc:
 "  - Developed this static website to house my portfolio and learn react js \n\n- Utilized github pages to host static website \n\n- Learned the correct Google domain anme system setup to have a custom domain name for this site"
     },
-  { name: "Linktree Inspired Website", title: "Front End Engineer", skills: "HTML, CSS, JS, Github Pages",
+  { name: "Linktree Inspired Website", title: "Full End Engineer", skills: "HTML, CSS, JS, Github Pages",
   desc:
 "  - Developed a static website with a landing page that has a list of widgets which link to whatever a user wants  \n\n- Implemented a copy link to clipboard button feature that doesnt also click the widget link itself \n\n- Developed different desktop and mobile friendly views"
       },
